f0646dd9d7fc376571761b7203074da1fea2da3f
Rob Vary <varygoode@gmail.com>
2020-08-05 03:58:39 -0500

    fixing string bugs in avatar command... again

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
 	if (!message.mentions.users.size) {
-		message.channel.send('Your avatar: ' + ${message.author.displayAvatarURL});
+		message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
 		}
 	else {
 		const avatarList = message.mentions.users.map(user => {
-		return ${user.username} + '\'s avatar: ' + ${user.displayAvatarURL};
+		return `${user.username} '\'s avatar: ${user.displayAvatarURL}`;
 		});
 
 		message.channel.send(avatarList);
