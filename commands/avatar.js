633d22aa1e52c107245759f0b5ae6351d9110f93
Rob Vary <varygoode@gmail.com>
2020-08-05 03:53:49 -0500

    trying new logic for avatar command... again

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,12 @@
 exports.run = (client, message, args) => {
-	if (message.content.startsWith('!avatar') {
-		if (!message.mentions.users.size) {
-		return message.channel.send('Your avatar: ${message.author.displayAvatarURL}');
+	if (!message.mentions.users.size) {
+		message.channel.send('Your avatar: ${message.author.displayAvatarURL}');
 		}
-
+	else {
 		const avatarList = message.mentions.users.map(user => {
 		return '${user.username}\'s avatar: ${user.displayAvatarURL}';
 		});
 
 		message.channel.send(avatarList);
-		}
+	}
 }
