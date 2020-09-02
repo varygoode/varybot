bf8f59104fc9e67e7931c8d63bed0cd63b4d5031
Rob Vary <varygoode@gmail.com>
2020-09-02 05:26:48 -0500

    working on autorole event

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -13,16 +13,20 @@ module.exports = (client, oldMember, newMember) => {
 
   	console.log(newMemberRoleMap.toString());
 
-  	if(newMemberRoleMap.has("<@&738516185611501670>") || newMemberRoleMap.has("<@&738516083753091073>")) {
-  		console.log("Winner");
-  	}
-
   	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
 	const removedRole = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
-	if (removedRole.size > 0) console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
+	if (removedRole.size > 0) {
+		console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
+
+	}
 
 	// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
 	const addedRole = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
-	if (addedRole.size > 0) console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
+	if (addedRole.size > 0) {
+		console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
+		if(addedRole.map.has(738516185611501670) || addedRole.map.has(738516083753091073)) {
+			console.log("Gottem");
+		}
+	}
 
 };
