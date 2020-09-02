011903396b8738e79ae20f0fbf5acbd5ff6b516d
Rob Vary <varygoode@gmail.com>
2020-09-02 04:26:55 -0500

    working on autorole event

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,5 +1,11 @@
 module.exports = (oldMember, newMember) => {
 
-  console.log(`${newMember.name} Changed`);
+  console.log(`${newMember} Changed`);
+
+  const newRole = newMember.roles.cache
+    .filter(r => !oldMember.roles.cache.has(r.id))
+    .first()
+
+  if(newRole) console.log(`${newRole} was added!`);
   
 };
