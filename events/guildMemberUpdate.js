d990339b924ca2a459dc1cb90fa0153673c761ed
Rob Vary <varygoode@gmail.com>
2020-09-02 06:09:11 -0500

    autorole event guild separation

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,33 +2,44 @@ const Discord = require('discord.js');
 
 module.exports = (client, oldMember, newMember) => {
 
-  	let separatorRoles1 = ["<@&738516185611501670>", "<@&738516083753091073>"];
+  	if(oldMember.guild.id == "732288737504919593") {
+	  	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+		const removedRole = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+		if (removedRole.size > 0) {
+			console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
+			if(!newMember.roles.cache.has("738516185611501670") && !newMember.roles.cache.has("738516083753091073")) {
+				if(newMember.roles.cache.has("750655898971537458")) newMember.roles.remove("750655898971537458");
+			}
+		}
 
-  	let newMemberRoleMap = newMember.roles.cache
-            .sort((a, b) => b.position - a.position)
-            .map(r => r)
-            .join(", ");
-            if (newMemberRoleMap.length > 1024) newMemberRoleMap = "Too many roles to display!";
-            if (!newMemberRoleMap) newMemberRoleMap = "No roles.";
-
-  	console.log(newMemberRoleMap);
-
-  	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
-	const removedRole = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
-	if (removedRole.size > 0) {
-		console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
-		if(!newMember.roles.cache.has("738516185611501670") && !newMember.roles.cache.has("738516083753091073")) {
-			if(newMember.roles.cache.has("750655898971537458")) newMember.roles.remove("750655898971537458");
+		// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
+		const addedRole = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+		if (addedRole.size > 0) {
+			console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
+			if(addedRole.has("738516185611501670") || addedRole.has("738516083753091073")) {
+				newMember.roles.add("750655898971537458");
+			}
 		}
 	}
 
-	// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
-	const addedRole = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
-	if (addedRole.size > 0) {
-		console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
-		if(addedRole.has("738516185611501670") || addedRole.has("738516083753091073")) {
-			newMember.roles.add("750655898971537458");
+	if(oldMember.guild.id == "434542839054204928") {
+	  	/* If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+		const removedRole = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+		if (removedRole.size > 0) {
+			console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
+			if(!newMember.roles.cache.has("738516185611501670") && !newMember.roles.cache.has("738516083753091073")) {
+				if(newMember.roles.cache.has("750655898971537458")) newMember.roles.remove("750655898971537458");
+			}
 		}
+
+		// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
+		const addedRole = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+		if (addedRole.size > 0) {
+			console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
+			if(addedRole.has("738516185611501670") || addedRole.has("738516083753091073")) {
+				newMember.roles.add("750655898971537458");
+			}
+		}*/
 	}
 
 };
