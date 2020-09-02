cd7eede98ea311313545019a9fa93f9e8ea2452a
Rob Vary <varygoode@gmail.com>
2020-09-02 03:00:23 -0500

    avatar command update

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,12 +1,4 @@
 exports.run = (client, message, args) => {
-	if (!message.mentions.users.size) {
-		message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
-		}
-	else {
-		const avatarList = message.mentions.users.map(user => {
-		return `${user.username}\'s avatar: ${user.displayAvatarURL}`;
-		});
-
-		message.channel.send(avatarList);
-	}
+	
+    message.reply(message.author.displayAvatarURL());
 }
--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,3 @@
 exports.run = (client, message, args) => {
     message.channel.send("pong!").catch(console.error);
-    message.reply(message.author.displayAvatarURL());
 }
