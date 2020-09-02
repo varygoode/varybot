2cc9f6de83a7b3c1dd15c9a253126ba683465886
Rob Vary <varygoode@gmail.com>
2020-09-02 04:38:45 -0500

    working on autorole event

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,21 +2,12 @@ const Discord = require('discord.js');
 
 module.exports = (client, oldMember, newMember) => {
 
-  const messagechannel = msg.guild.channels.find('name', 'general');
-        if (oldMember.roles.size < newMember.roles.size) {
-            const embed = new Discord.RichEmbed()
-                .setColor('#FE2E2E')
-                .setTimestamp()
-                .setAuthor('Role added!')
-                .addField(`Member:`, `${oldMember.user.tag} (${oldMember.id})`);
-            for (const role of newMember.roles.map(x => x.id)) {
-                if (!oldMember.roles.has(role)) {
-                    embed.addField(`Role:`, `${oldMember.guild.roles.get(role).name}`);
-                }
-            }
-            messagechannel.send({
-                embed
-            });
-        }
+  if (oldMember.roles.size > newMember.roles.size) {
+    console.log(`Role added`);
+  }
+
+  if (oldMember.roles.size < newMember.roles.size) {
+    console.log(`Role removed`);
+  }
   
 };
