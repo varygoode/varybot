3ac89cbef5f8b36162957d5be46ae6515e4fc0dc
Rob Vary <varygoode@gmail.com>
2020-09-02 10:46:12 -0500

    role refresh command

--- commands/rolerefresh.js
+++ commands/rolerefresh.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-    message.guild.members.filter(m => !m.user.bot).forEach(member => member.roles.add("738516185611501670"));
-    message.guild.members.filter(m => !m.user.bot).forEach(member => member.roles.remove("738516185611501670"));
+    message.guild.members.cache.filter(m => !m.user.bot).forEach(member => member.roles.add("738516185611501670"));
+    message.guild.members.cache.filter(m => !m.user.bot).forEach(member => member.roles.remove("738516185611501670"));
 }
