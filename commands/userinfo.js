a170fe44100fd91b39cd48ad229da0b342674efe
Rob Vary <varygoode@gmail.com>
2020-09-02 03:12:01 -0500

    working on userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,20 @@
 exports.run = (bot, message, args) => {
   message.channel.send("gottem").catch(console.error);
+
+  // Checks if a user is mentioned
+  let ment = message.mentions.users.first();
+    if(!ment) {
+      message.channel.send('Please mention a user!')
+    }
+  // Creats an embed with information about the mentioned user
+    let embed = new Discord.RichEmbed()
+    .addField("Username", ment.tag)
+    .addField("ID", ment.id)
+    .addField("Status", ment.presence.status)
+    .addField("Created", ment.createdAt)
+    .setThumbnail(ment.avatarURL)
+    message.channel.send(embed)
+  // Displays a message in the console if the command was used
+    return console.log(`> userinfo command used by ${message.author.username}`);
+  }
 }
