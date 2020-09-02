6345440b98955ad2dbcb9722d16236b075f52533
Rob Vary <varygoode@gmail.com>
2020-09-02 03:13:49 -0500

    working on userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,20 +1,25 @@
-exports.run = (bot, message, args) => {
-  message.channel.send("gottem").catch(console.error);
+module.exports.run = async (client, message, args) => {
+  let guildMember;
 
-  // Checks if a user is mentioned
-  let ment = message.mentions.users.first();
-    if(!ment) {
-      message.channel.send('Please mention a user!')
-    }
-  // Creats an embed with information about the mentioned user
-    let embed = new Discord.RichEmbed()
-    .addField("Username", ment.tag)
-    .addField("ID", ment.id)
-    .addField("Status", ment.presence.status)
-    .addField("Created", ment.createdAt)
-    .setThumbnail(ment.avatarURL)
-    message.channel.send(embed)
-  // Displays a message in the console if the command was used
-    return console.log(`> userinfo command used by ${message.author.username}`);
+  if (message.mentions.members.first()) {
+    guildMember = message.mentions.members.first();
+  } else {
+    guildMember = message.member;
   }
+
+  // We need the User object aswell for different properties
+  const user = guildMember.user;
+
+  let embed = new Discord.RichEmbed()
+    .setAuthor(user.username)
+    .setDescription("Users Info", true)
+    .setColor("#64FF00", true)
+    .addField("Full Username:", `${user.username}${user.discriminator}`, true)
+    .addField("ID:", user.id, true)
+    .addField("Created at:", user.createdAt, true)
+    .addField("Status:", `${user.presence.status}`, true)
+    .addField("Game:", `${user.presence.game}`, true)
+    .addField("Roles", guildMember.roles.map(r => `${r}`).join('|'), true);
+
+  message.channel.send(embed);
 }
