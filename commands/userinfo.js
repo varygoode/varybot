842f7e927defeb0baf066d18b90885f4d974cef4
Rob Vary <varygoode@gmail.com>
2020-09-02 03:34:06 -0500

    working on userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -18,12 +18,12 @@ module.exports.run = async (client, message, args) => {
     .setAuthor(user.username)
     .setDescription("Users Info", true)
     .setColor("#64FF00", true)
-    .addField("Full Username:", `${user.username}${user.discriminator}`, true)
+    .addField("Full Username:", `${user.username}#${user.discriminator}`, true)
     .addField("ID:", user.id, true)
     .addField("Created at:", user.createdAt, true)
     .addField("Status:", `${user.presence.status}`, true)
     .addField("Game:", `${user.presence.game}`, true)
-    /*.addField("Roles", guildMember.roles.map(r => `${r}`).join('|'), true)*/;
+    .addField("Roles", guildMember.roles.map(role => role.name).join('|'), true);
 
   message.channel.send(embed);
 }
