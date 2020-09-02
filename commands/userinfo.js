2fe47d2182058b561b5813c823cb04ca8f772359
Rob Vary <varygoode@gmail.com>
2020-09-02 03:11:16 -0500

    working on userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,27 +1,3 @@
 exports.run = (bot, message, args) => {
-  message.channel.send("gottem");
-
-  let guildMember;
-
-  if (message.mentions.members.first()) {
-    guildMember = message.mentions.members.first();
-  } else {
-    guildMember = message.member;
-  }
-
-  // We need the User object aswell for different properties
-  const user = guildMember.user;
-
-  let embed = new Discord.RichEmbed()
-    .setAuthor(user.username)
-    .setDescription("Users Info", true)
-    .setColor("#64FF00", true)
-    .addField("Full Username:", `${user.username}${user.discriminator}`, true)
-    .addField("ID:", user.id, true)
-    .addField("Created at:", user.createdAt, true)
-    .addField("Status:", `${user.presence.status}`, true)
-    .addField("Game:", `${user.presence.game}`, true)
-    .addField("Roles", guildMember.roles.map(r => `${r}`).join('|'), true);
-
-  message.channel.send(embed);
+  message.channel.send("gottem").catch(console.error);
 }
