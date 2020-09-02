ddeb9c44232356dae588553db58463a13097af7e
Rob Vary <varygoode@gmail.com>
2020-09-02 05:00:14 -0500

    working on autorole event

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 
 module.exports = (client, oldMember, newMember) => {
 
-  	console.log(`${oldMember.displayName}`);
+  	console.log(oldMember.roles.cache.toString());
 
   	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
-	const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
-	if (removedRoles.size > 0) console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
+	const removedRole = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+	if (removedRoles.size > 0) console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
 	// If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
-	const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
-	if (addedRoles.size > 0) console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+	const addedRole = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+	if (addedRoles.size > 0) console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
 
 };
