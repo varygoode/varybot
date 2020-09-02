013d50494e8baf6c8b8fa56e33982e74f33faadf
Rob Vary <varygoode@gmail.com>
2020-09-02 04:43:34 -0500

    working on autorole event

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 module.exports = (oldMember, newMember) => {
 
-  if (oldMember.cache.roles.size > newMember.cache.roles.size) {
+  if (oldMember.roles.cache.size > newMember.roles.cache.size) {
     console.log(`Role added`);
   }
 
-  if (oldMember.cache.roles.size < newMember.cache.roles.size) {
+  if (oldMember.roles.cache.size < newMember.roles.cache.size) {
     console.log(`Role removed`);
   }
   
