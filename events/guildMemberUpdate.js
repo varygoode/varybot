7d5767beeb366dd6201cfb227bfa608bcbc36f38
Rob Vary <varygoode@gmail.com>
2020-09-02 10:10:19 -0500

    autorole fix role mixup

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -55,14 +55,14 @@ module.exports = (client, oldMember, newMember) => {
 		if (addedRole.size > 0) {
 			console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
 			//Add GAME role separator
-			if(newMember.roles.cache.has("743625569551253636") || newMember.roles.cache.has("693214736736714762") ||
+			if(newMember.roles.cache.has("693215135053119568") || newMember.roles.cache.has("693214736736714762") ||
 			   newMember.roles.cache.has("693214952944828426") || newMember.roles.cache.has("749484771146465321") ||
 			   newMember.roles.cache.has("738523221946794145")) {
 			   	console.log("GAMES separator being added");
 				newMember.roles.add("742637482100785186");
 			}
 			//Add NERDS role separator
-			if(newMember.roles.cache.has("693215135053119568") || newMember.roles.cache.has("743481813972025405") ||
+			if(newMember.roles.cache.has("743625569551253636") || newMember.roles.cache.has("743481813972025405") ||
 			   newMember.roles.cache.has("743484247255285783") || newMember.roles.cache.has("750675217004888124") ||
 			   newMember.roles.cache.has("743482189760692237")) {
 			   	console.log("NERDS separator being added");
