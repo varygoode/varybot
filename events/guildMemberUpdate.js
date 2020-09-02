4131590a5e236df9361b5df87f5a1ab9a0ff31f3
Rob Vary <varygoode@gmail.com>
2020-09-02 06:13:00 -0500

    autorole event games separator

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -23,12 +23,14 @@ module.exports = (client, oldMember, newMember) => {
 	}
 
 	if(oldMember.guild.id == "434542839054204928") {
-	  	/* If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+	  	// If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
 		const removedRole = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 		if (removedRole.size > 0) {
 			console.log(`The role ${removedRole.map(r => r.name)} was removed from ${oldMember.displayName}.`);
-			if(!newMember.roles.cache.has("738516185611501670") && !newMember.roles.cache.has("738516083753091073")) {
-				if(newMember.roles.cache.has("750655898971537458")) newMember.roles.remove("750655898971537458");
+			if(!newMember.roles.cache.has("693215135053119568") && !newMember.roles.cache.has("693214736736714762") &&
+			   !newMember.roles.cache.has("693214952944828426") && !newMember.roles.cache.has("749484771146465321") &&
+			   !newMember.roles.cache.has("738523221946794145")) {
+				if(newMember.roles.cache.has("742637482100785186")) newMember.roles.remove("742637482100785186");
 			}
 		}
 
@@ -36,10 +38,12 @@ module.exports = (client, oldMember, newMember) => {
 		const addedRole = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
 		if (addedRole.size > 0) {
 			console.log(`The role ${addedRole.map(r => r.name)} was added to ${oldMember.displayName}.`);
-			if(addedRole.has("738516185611501670") || addedRole.has("738516083753091073")) {
-				newMember.roles.add("750655898971537458");
+			if(!newMember.roles.cache.has("693215135053119568") || !newMember.roles.cache.has("693214736736714762") ||
+			   !newMember.roles.cache.has("693214952944828426") || !newMember.roles.cache.has("749484771146465321") ||
+			   !newMember.roles.cache.has("738523221946794145")) {
+				newMember.roles.add("742637482100785186");
 			}
-		}*/
+		}
 	}
 
 };
